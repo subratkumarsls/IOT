2bf382d69df4af1a9cd5d024c393e5701e86223c
Subrat <subratkumarsls.123@gmail.com>
2025-07-22 00:16:17 +0530

    day2

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'homepage.dart';
+
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      
-    );
+    return MaterialApp(home: Homepage());
   }
 }
